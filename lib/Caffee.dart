5eed017fada3e8ed03d0d61f9338de5d7e54c79f
101220843-afk <101220843@mu.edu.lb>
2025-11-10 16:06:12 +0200

    Caffee.dart

--- lib/Caffee.dart
+++ lib/Caffee.dart
@@ -1,48 +1,83 @@
 import 'package:flutter/material.dart';
 
-abstract class Vote_Tea extends StatefulWidget {
-  const Vote_Tea({super.key});
-
+class Caffe extends StatefulWidget {
+  const Caffe({super.key});
 
   @override
-  State<Vote_Tea> createState();
-  return Vote_TeaState();
-  
-}
-class Vote_Tea extends State<Vote_Tea>{
-  String sentence="❔";
-  String sentence = "Tea or Coffe";
-  int Tea_Votes=0;
-  int Coffee_Votes=0;
-  
-
-  void Vote_Tea(){
-    setState(() {
-      sentence="🍵";
-      Tea_Votes++;
-    });
-    
-  void Vote_Coffee(){
-    setState(() {
-      sentence="☕";
-      Coffee_Votes++;
-    });
+  State<Caffe> createState() {
+    return _CaffeState();
   }
 }
 
-@override
-Widget build(BuildContext cotext){
-  return Scaffold(backgroundColor: Colors.white,
-  body: Column(mainAxisAlignment:MainAxisAlignment.center
-  children:[
-    Center(child: Text('Tea or Coffe'),style:TextStyle(fontSize: 40)),
-    SizedBox(height: 20),
-    child:Text('$sentence',style:TextStyle(fontSize: 40)),
-      SizedBox(height: 20);
-      Row(MainAxisAlignment:MainAxisAlignment.center,
+class _CaffeState extends State<Caffe> {
+  String userChoice = '❓';
+  int teaChoice = 0;
+  int coffeChoice = 0;
 
-      children: [ElevatedButton(onPressed: Vote_Tea, child: Text()))];
+  void tea() {
+    setState(() {
+      userChoice = '🍵';
+      teaChoice++;
+    });
+  }
 
-}
-}
+  void coffe() {
+    setState(() {
+      userChoice = '☕';
+      coffeChoice++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('${userChoice}', style: TextStyle(fontSize: 50))),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: tea,
+                child: Text(
+                  'Vote Tea',
+                  style: TextStyle(fontSize: 20),
+                  selectionColor: Colors.green,
+                ),
+              ),
+
+              SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: coffe,
+                child: Text(
+                  'Vote Coffe',
+                  style: TextStyle(fontSize: 20),
+                  selectionColor: Colors.brown,
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                userChoice = '❓';
+                teaChoice = 0;
+                coffeChoice = 0;
+              });
+            },
+            child: Text('Reset', style: TextStyle(fontSize: 20)),
+          ),
+          SizedBox(height: 20),
+          Text('Tea Votes: ${teaChoice}', style: TextStyle(fontSize: 20)),
+          Text('Coffe Votes: ${coffeChoice}', style: TextStyle(fontSize: 20)),
+        ],
+      ),
+    );
+  }
 }
